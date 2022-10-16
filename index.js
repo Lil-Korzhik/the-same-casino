b83e91435f5a2663788af5bc727018d9b093fb56
Lil-Korzhik <mr.purple2007@gmail.com>
2022-10-16 21:43:37 +0300

    Add Start CallBack

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.start(ctx => {
+    const chatId = ctx.message.chat.id,
+          message = `Hello, ${ctx.from.first_name}!`;
+
+    ctx.telegram.sendMessage(chatId, message);
+});
+
 bot.launch().then(() => {
     console.log('[LOG] | Bot launched successfully!');
 });
